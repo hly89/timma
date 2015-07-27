5400b478225bd930d8b162c4798ed6b41bd3c39e
Liye He <heliye89@gmail.com>
2015-07-27 16:03:22 +0300

    visualize in cytoscape

--- visualize_net_cytoscape.R
+++ visualize_net_cytoscape.R
@@ -1,13 +1,33 @@
-#------------------
-# Visualization in Cytoscape 2.8/3.0
-#------------------
-VisualizeCytoscape <- function (graph) {
-	# the edge list in EntrezID
-	edge.list.entrez <- get.edgelist(graph)
-	# the edge list in Symbol
-	nodes.entrez <- V(graph)$EntrezID
-	nodes.symbol <- V(graph)$Symbol
-	edge.list.symbol <- apply(edge.list.entrez, c(1,2), function(x) nodes.symbol[which(nodes.entrez == x)])
+VisualizeNetCYT <- function(graph, mode = "automatic") {
+	#------------------
+	# Visualization in Cytoscape 2.8/3.0
+	#------------------
+	if (!(mode %in% c("automatic", "manual"))) {
+		stop ("The mode parameter can only be either automatic or manual!")
+	}
+  # the required attributes for graph: node.fillColor, node.shape
+	nodes.num <- vcount(graph)
+	# no node.fillColor attributes
+	if (is.null(V(graph)$node.fillColor)) {
+		graph <- set.vertex.attribute(graph, "node.fillColor", value = "84,84,84") # default color
+	} 
+	
+	# node shape 
+	shape.style <- c('ellipse','rectangle','triangle','diamond','hexagon','octagon','parallelogram','roundrect','vee')
+	variable.type.list <- c("timma_parental","timma_tam","timma_parental timma_tam","cnv","exp","mutation","others")
+	nodes.type <- c()
+	nodes.alteration <- V(graph)$Alterations
+	for (i in seq_len(nodes.num)) {
+	  tmp <- which(variable.type.list == nodes.alteration[i])
+	  if (length(tmp) == 0) {
+	    nodes.type[i] <- shape.style[7]
+	  } else {
+	    nodes.type[i] <- shape.style[tmp]
+	  }
+	}
+	graph <- set.vertex.attribute(graph, "node.shape", value = nodes.type)
+	
+	
 	
 	# generate edge color
 	edges.type <- E(graph)$kind
@@ -16,48 +36,85 @@ VisualizeCytoscape <- function (graph) {
 	edges.color[grep("Activation", edges.type)] <- "255,0,0" # red edges for activation
 	edges.color[grep("Inhibition", edges.type)] <- "0,0,255" # blue edges for inhibition
 	
-	# generate edge width
-	edges.width <- E(graph)$weight
-	edges.width <- paste("w:", edges.width, sep = "")
+	graph <- set.edge.attribute(graph, "edge.color", value = edges.color)
+	
 	# generate edge arrow
 	edges.arrow <- vector(mode = "character", length = length(edges.type))
 	edges.arrow[grep("Interaction", edges.type)] <- "NONE" 
 	edges.arrow[grep("Activation", edges.type)] <- "ARROW" 
-	edges.arrow[grep("Inhibition", edges.type)] <- "T" 
-	# generate the edge attributes file
-	edges.attr.table <- cbind(edge.list.symbol, edges.color, edges.width, edges.arrow)
-	colnames(edges.attr.table) <- c("source", "target", "edge.color", "edge.label", "edge.targetArrowShape")
+	edges.arrow[grep("Inhibition", edges.type)] <- "T"
 	
-	write.table(edges.attr.table, file = "edge.attributes.cytoscape.txt", row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
+	graph <- set.edge.attribute(graph, "edge.targetArrowShape", value = edges.arrow)
 	
-	# generate node attributes file: a table with columns as attributes
-	# node color
-	nodes.num <- vcount(graph)
-	if (is.null(V(graph)$color)) {
-		nodes.color <- rep("84,84,84", nodes.num)
-	} else {
-		nodes.color <- V(graph)$color
-	}
+	# generate edge line width
+	graph <- set.edge.attribute(graph, "edge.lineWidth", value = E(graph)$weight)
 	
-	# node shape 
-	shape.style <- c('ellipse','rectangle','triangle','diamond','hexagon','octagon','parallelogram','roundrect','vee')
-	variable.type.list <- c("timma_parental","timma_tam","timma_parental timma_tam","cnv","exp","mutation","others")
-	nodes.type <- c()
-	nodes.alteration <- V(graph)$Alterations
-	for (i in seq_len(nodes.num)) {
-		tmp <- which(variable.type.list == nodes.alteration[i])
-		if (length(tmp) == 0) {
-			nodes.type[i] <- shape.style[7]
+	
+	if (mode == "automatic") {
+		# convert to graphNEL 
+		graph.cyt <- igraph.to.graphNEL(graph)
+	
+		# create attributes for graphNEL
+		graph.cyt <- initNodeAttribute(graph.cyt, 'Symbol', 'char', "symbol")
+		#graph.cyt <- initNodeAttribute(graph.cyt, 'name', 'char', "symbol")	
+		graph.cyt <- initNodeAttribute(graph.cyt, 'EntrezID', 'numeric', 0)
+		graph.cyt <- initNodeAttribute(graph.cyt, 'Alterations', 'char', 'exp')
+		graph.cyt <- initNodeAttribute(graph.cyt, 'node.fillColor', 'char', '84,84,84')
+		graph.cyt <- initNodeAttribute(graph.cyt, 'node.shape', 'char', 'ellipse')
+		graph.cyt <- initNodeAttribute(graph.cyt, 'Value', 'numeric', 0)
+		graph.cyt <- initEdgeAttribute(graph.cyt, 'weight', 'numeric', 0)
+	    graph.cyt <- initEdgeAttribute(graph.cyt, 'kind', 'char', 'interaction')
+	    graph.cyt <- initEdgeAttribute(graph.cyt, 'edge.color', 'char', '255,0,0')
+	    graph.cyt <- initEdgeAttribute(graph.cyt, 'edge.targetArrowShape', 'char', 'NONE')
+	    graph.cyt <- initEdgeAttribute(graph.cyt, 'edge.lineWidth', 'char', '1')
+		# start the clock
+		ptm <- proc.time()
+		os.type <- Sys.info()["sysname"]
+		if (os.type == "Darwin") { # mac os
+			system("open -a /Applications/Cytoscape_v2.8.2/Cytoscape.app")
+			# check if the port is open
+			port.info <- suppressWarnings(system("lsof -i :9000", intern = TRUE))
+			while (length(port.info) == 0) {
+				port.info <- suppressWarnings(system("lsof -i :9000", intern = TRUE))
+				running.time <- proc.time() - ptm
+				if (running.time[3] > 180) {
+					stop ("Taking too much time to open Cytoscape, please open Cytoscape manually and run again!")
+				}
+			}
+		    cyt.window <- new.CytoscapeWindow("Network", graph = graph.cyt, overwriteWindow = TRUE)
+			displayGraph(cyt.window)
+		    cy <- CytoscapeConnection ()
+		    layout.name <- getLayoutNames(cy)[15]
+		    layoutNetwork(cyt.window, layout.name)
+		    setDefaultNodeSize(cyt.window, 70)
+			redraw(cyt.window)
 		} else {
-			nodes.type[i] <- shape.style[tmp]
+		    cyt.window <- new.CytoscapeWindow("Network", graph = graph.cyt, overwriteWindow = TRUE)
+			displayGraph(cyt.window)
+		    cy <- CytoscapeConnection ()
+		    layout.name <- getLayoutNames(cy)[15]
+		    layoutNetwork(cyt.window, layout.name)
+		    setDefaultNodeSize(cyt.window, 70)
+			redraw(cyt.window)
 		}
+	    
+		
+	    
+	} else {
+		# the edge list in Symbol
+		edge.list.symbol <- get.edgelist(graph)
+		# the edge list in Symbol
+		nodes.entrez <- V(graph)$EntrezID
+		nodes.symbol <- V(graph)$Symbol
+		#edge.list.symbol <- apply(edge.list.entrez, c(1,2), function(x) nodes.symbol[which(nodes.entrez == x)])
+		edges.attr.table <- cbind(edge.list.symbol, edges.color, E(graph)$weight, edges.arrow)
+		colnames(edges.attr.table) <- c("source", "target", "edge.color", "edge.Width", "edge.targetArrowShape")
+		write.table(edges.attr.table, file = "edge.attributes.cytoscape.txt", row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
+		# generate the node attribute table
+		node.attr.table <- cbind(nodes.symbol, nodes.type, nodes.alteration, V(graph)$node.fillColor, V(graph)$Value)
+		colnames(node.attr.table) <- c("name", "node.shape", "type", "node.fillColor", "value")
+		write.table(node.attr.table, file = "node.attributes.cytoscape.txt", row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
 	}
-	# generate the node attribute table
-	node.attr.table <- cbind(nodes.symbol, nodes.type, nodes.alteration, nodes.color, V(graph)$Value)
-	colnames(node.attr.table) <- c("name", "node.shape", "type", "node.fillColor", "value")
 	
-	write.table(node.attr.table, file = "node.attributes.cytoscape.txt", row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
+
 }
-
-
-
